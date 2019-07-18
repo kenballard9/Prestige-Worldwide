3b8ac71b73453de0452512b467f98bbe20d48938
Ballard <JKB6146@yum.com>
2019-07-18 19:33:18 -0400

    Added Comments to Script.js file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
-/*$(window).scroll(function() {
-    if ($(this).scrollTop() > 0) {
-      $('.myNav').fadeOut();
-    } else {
-      $('.myNav').fadeIn();
-    }
-  });*/
+//This is the Jquery function responsible for making the navigation bar disappear when scrolling down the screen
+//and reappearing once the page is scrolled to the top.
 
   $(window).scroll(function(){
     $(".myNav").css("opacity", 1 - $(window).scrollTop() / 250);
